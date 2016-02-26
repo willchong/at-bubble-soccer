4a92d64c9a47d8688d0f438b16f62fda6c3101f4
William Chong <williamc@tribalworldwide.ca>
2016-02-25 23:18:16 -0500

    more video updates

--- src/js/script.js
+++ src/js/script.js
@@ -1,124 +1,155 @@
- var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video', {
-    height: '100%',
-    width: '100%',
-    videoId: 'RpHZPJJcyHg',
-    playerVars: {
-        'rel': 0,
-        'showinfo': 0,
-        'modestbranding': 1,
-        'controls': 0,
-        'fs': 0,
-        'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-function onPlayerReady(event) {
-
-}
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var stopped = false;
-function onPlayerStateChange(event) {
-      if (event.data == YT.PlayerState.PLAYING) {
-        stopped = false;  
-        $('.play-pause').css({'background-position-y': '-24px'});
-      } else {      
-        stopped = true;
-        $('.play-pause').css({'background-position-y': '0px'});
-      }
-}
-function stopVideo() {
-  player.stopVideo();
-}
-
 $('.down-arrow').on('click', function() {
 
     //scroll to next section
+    // var body = $("html, body");
+    // var position = $("section.intro-hex").offset().top;
+    // body.stop().animate({scrollTop:position}, '500', 'swing');
+    $.scrollify.move("#intro");
 
+});
+
+$('nav').on('click', 'a', function(event) {
+
+    //scroll to next section
+    var check = $(event.target).closest('a').hasClass('social');
+    if (check == false) {
+        event.preventDefault();
+        $.scrollify.disable();
+        setTimeout(reEnable, 750);
+        var s = $(event.target).attr('data-scroll');
+        var position = $("."+s).offset().top;
+        var body = $("html, body");
+        window.location.hash = "";
+        body.stop().animate({scrollTop:position}, '500', 'swing');
+    }
+   
+
+});
+
+function reEnable() {
+    console.log('wut');
+    $.scrollify.enable();
+}
+
+var myPlayer;
+
+videojs("video").ready(function(){
+  myPlayer = this;
+
+  // var _video = document.getElementById('video');
+  // _video.addEventListener('playing', videoPlaying, false);
+
+});
+
+
+// function videoPlaying(e) {
+//     console.log('playing');    
+// };
+
+$('#video_html5_api').on('play', function (e) {
+    $('.play-pause').css({'background-position-y': '-24px'});
+});
+
+$('#video_html5_api').on('pause', function (e) {
+    $('.play-pause').css({'background-position-y': '0px'});
 });
 
 $('.play-pause').on('click', function() {
 
-    if (stopped == false) {
-        player.pauseVideo();
-    } else if (stopped == true) {
-        player.playVideo();
+    if (myPlayer.paused() == false) {
+        myPlayer.pause();
+    } else if (myPlayer.paused() == true) {
+        myPlayer.play();
     }
 
 });
 
 $('.sound').on('click', function() {
 
-    if (!player.isMuted()) {
-        player.mute();
+    if (myPlayer.volume() == 1) {
+        myPlayer.volume(0);
         $('.sound').css({'background-position-y': '-24px'});
-    } else if (player.isMuted()) {
-        player.unMute();
+    } else if (myPlayer.volume() == 0) {
+        myPlayer.volume(1);
         $('.sound').css({'background-position-y': '0px'});
     }
 
 });
 
+function isElementInViewport (el) {
 
-var buffer = 300;
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
 
 $(window).on('scroll', function(){
 
     var currentPos = $(window).scrollTop();
-    console.log(currentPos);
 
-    if (currentPos >= ($('section.intro-hex').offset().top - buffer)) {
+    if (isElementInViewport($('section.intro-hex'))) {
         $('.intro-hex .hex').addClass('fadein');
-    } 
-    if (currentPos >= ($('section.middle-hex').offset().top - buffer)) {
+    } else if (isElementInViewport($('section.middle-hex'))) {
         $('.middle-hex .hex').addClass('fadein');
-    }
-    if (currentPos >= ($('section.bottom-hex').offset().top - buffer)) {
+    } else if (isElementInViewport($('section.bottom-hex'))) {
         $('.bottom-hex .hex').addClass('fadein');
     }
 
+});
+
+$(document).on('ready', function(){
+
+    $('#map').css({'pointer-events':'none'});
+
+    $('.map').on('click', function() {
+
+        $('#map').css({'pointer-events':'auto'});
+
+    });
+
+    var windowWidth = $(window).width();
+    var calculatedHeight = windowWidth/(16/9);
+    console.log(calculatedHeight);
+    $('section.video').attr('style', 'height:'+calculatedHeight+'px !important');
+
+    $.scrollify({
+        section : ".scrollify",
+        sectionName : "section-name",
+        standardScrollElements : ".about, .info, .contact-us"
+    });
 
 });
 
-// When the window has finished loading create our google map below
+$(window).on('resize', function() {
+    var windowWidth = $(window).width();
+    var calculatedHeight = windowWidth/(16/9);
+    console.log(calculatedHeight);
+    $('section.video').attr('style', 'height:'+calculatedHeight+'px !important');
+});
+
 google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
+        zoom: 14,
         center: new google.maps.LatLng(43.7853341, -79.4838277),
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(43.7853341, -79.4838277),
         map: map,
